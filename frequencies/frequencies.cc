4e9d18e89fde52e351264264d0eb16cdb2fcd0e4
Boyi Zhang <bz11252@uga.edu>
2017-07-06 14:58:44 -0400

    working version of frequencies script

--- frequencies/frequencies.cc
+++ frequencies/frequencies.cc
@@ -1,7 +1,21 @@
 #include <iostream>
 #include "../molecule_analysis/molecule.h"
-#include "../molecule_analysis/masses.h"
+#include "../extra_files/masses.h"
 #include <cmath>
+
+//Uses Eigen package (canned library) that is used to manipulate matrix types
+#include "../extra_files/Eigen/Dense" 
+#include "../extra_files/Eigen/Eigenvalues"
+#include "../extra_files/Eigen/Core"
+
+#define _hartoJ 4.3597443e-18
+#define _amutokg (1/1.6605389e-27)
+#define _bohrtom (1/5.2917721e-11)
+#define _c 2.99792458e10
+#define _pi acos(-1.0)
+
+//defines new type called matrix that's dynamically allocated and contains only doubles 
+typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> Matrix; 
 
 using namespace std;
 
@@ -22,19 +36,19 @@ int main(int argc, char *argv[])
     fscanf(hessian, "%d", &natom); //fscanf reads in a regex from pointer to FILE hessian and saves it to the address of natom
 
     //test to see if the number of atoms in the geom.dat and hessian.dat file is consistent
-    if (mol.natom!=natom) printf("Warning: number of atoms inconsistent between hessian and input files\n");
+    if (mol.natom!=natom) printf("Warning: number of atoms inconsistent between hessian and geom input files\n");
 
     //allocate memory for the hessian matrix made of doubles, by defining H as ** we can call H in 2 dimensions  
     double **H = new double*[3*natom]; //the size is 3N for the x,y,z coordinates of each atom
-    double **H_M = new double*[3*natom]; //mass weighted hessian matrix
+    Matrix H_M(3*natom,3*natom); //define mass-weighted hessian matrix object
+
     for(int i=0; i < 3*natom; i++){ 
         H[i] = new double[3*natom];
-        H_M[i] = new double[3*natom];
     }
     
     //for loops will go through each line in hessian.dat in order that contains format specified 
     for(unsigned int i=0; i < 3*natom; i++) { 
-        for(unsigned int j=0; j < 3; j++) {
+        for(unsigned int j=0; j < natom; j++) {
             
             //puts hessian.dat in a square matrix 3N by 3N. First three rows read correspond to first row of square matrix, etc.
             fscanf(hessian, "%lf %lf %lf", &H[i][j*3], &H[i][j*3+1],&H[i][j*3+2]); //1st row in places j=0-2, 2nd in places j=3-5, 3rd in places j=6-8
@@ -50,26 +64,32 @@ int main(int argc, char *argv[])
   //                  printf("value: %lf zvals i: %d zvals j: %d i: %d j: %d \n", H[k][l], int (mol.zvals[i]),  int (mol.zvals[j]),i,j);
                         
             
-                    H_M[k][l] = H[k][l]/sqrt(mass[(int) mol.zvals[i]]*mass[(int) mol.zvals[j]]);
+                    H_M(k,l) = H[k][l]/sqrt(mass[(int) mol.zvals[i]]*mass[(int) mol.zvals[j]]);
                 }
             }
         }
     }
 
-    
-    for(unsigned int i=0; i < 3*natom; i++) { 
-        printf("%lf %lf %lf %lf %lf %lf %lf %lf %lf \n", H_M[i][0], H_M[i][1], H_M[i][2], H_M[i][3], H_M[i][4], H_M[i][5], H_M[i][6], H_M[i][7], H_M[i][8]);
+    cout << "Weighted Hessian Matrix: \n";
+    cout << H_M << endl;
+
+    Eigen::SelfAdjointEigenSolver<Matrix> solver(H_M);
+    Matrix evecs = solver.eigenvectors();
+    Matrix evals = solver.eigenvalues(); //automatically puts in a sorted array smallest first
+
+    cout << "Eigenvalues (hartree/amu-bohr^2):\n";
+    cout << evals << endl;
+
+    cout << "Frequencies(cm^-1):\n";
+    //cout << pow(evals, 0.5)*8065.54446 << endl;
+    for (int i=0; i<3*natom; i++) {
+        cout << sqrt(evals(i)*_hartoJ*_amutokg*_bohrtom*_bohrtom)*(1/(2*_pi))*(1/_c) << endl;
     }
-
-
-
     //frees allocated block of memory for H and H_M
     for (int i=0; i<3*natom; i++) {
         delete[] H[i];
-        delete[] H_M[i];
     }
     delete[] H;
-    delete[] H_M;
 
     return 0;
 }  

3012fada688b0f0e6427a2b7a1060b6dc921b2a5
Boyi Zhang <bz11252@uga.edu>
2017-07-06 13:42:49 -0400

    contains files needed

---
+++ extra_files/masses.h
@@ -0,0 +1,14 @@
+
+using namespace std; //using standard namespace 
+
+static const double mass[] = {
+0.000000,
+1.007825,
+4.002603,
+7.016003,
+9.012183,
+11.009305,
+12.000000,
+14.003074,
+15.994914  
+};
